5ace39341559b39aee7ada98cd2367dab0b1cb54
Anu Mishra <anumishra8@deloitte.com>
2017-08-23 16:16:18 +0530

    Initial Commit

---
+++ ProgressTask/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProgressTask
+//
+//  Created by Mishra, Anu (US - Mumbai) on 8/23/17.
+//  Copyright © 2017 Mishra, Anu (US - Mumbai). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
